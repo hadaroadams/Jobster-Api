298d1c8ab12b29aa39e92f7e0d1359cf0e9491e4
hadaroadams <hadaroadams1234@gmail.com>
2024-02-25 08:46:15 +0000

    completed jobster api v1

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
 const app = express();
+// extra security
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+// Swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
 const mongoose = require("mongoose");
 const connectDB = require("./config/dbconn");
 const errorHandle = require("./middleware/errorHandle");
@@ -12,11 +21,23 @@ const authenticationMiddleWare = require("./middleware/authentication");
 
 connectDB();
 const PORT = process.env.PORT || 3900;
-// middle ware
+// const limiter = rateLimit({
+//   windowMS: 15 * 60 * 1000,
+//   max: 2,
+// });
+// middle
+//security
+app.set("trust proxy", 1);
+// app.use(limiter);
 
+//extra security
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
+app.use('/api/docs',swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 // routes
 app.use("/api/v1/auth", authApi);
 app.use("/api/v1/jobs", authenticationMiddleWare, jobApi);
--- middleware/errorHandle.js
+++ middleware/errorHandle.js
@@ -1,11 +1,32 @@
-const {CustomError} =require('./../errors/index')
-const {StatusCodes} = require('http-status-codes')
+const { CustomError } = require("./../errors/index");
+const { StatusCodes } = require("http-status-codes");
 
-const errorHandle = (err,req,res,next)=>{
-    if(err instanceof CustomError){
-        return res.status(err.status).json({message:err.message})
-    }
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message:"something went wrongf"})
-}
+const errorHandle = (err, req, res, next) => {
+  console.log(err);
+  let customError = {
+    statusCode: err.status || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "something went wrong try again later",
+  };
+  if (err.name === "ValidatorError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+    customError.statusCode = 400;
+  }
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplication value entered for ${Object.keys(
+      err.keyValue
+    )} field, Please choose another value`;
+    customError.statusCode = 400;
+  }
+  if (err.name === "CastError") {
+    customError.msg = `No item found with id :${err.value} `;
+    customError.stastusCode = 404;
+  }
+  return res
+    .status(customError.statusCode)
+    .json(customError.msg)
+    .json({ message: "something went wrongf" });
+};
 
-module.exports = errorHandle
+module.exports = errorHandle;
